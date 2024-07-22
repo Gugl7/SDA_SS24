b81eed1cb236bc40c547d0e7341f80e6f3e71480
e12016483 <e12016483@student.tuwien.ac.at>
2024-07-22 12:00:38 +0200

    adding elimination orderings

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # File structure
 
-- data folder: contains input and output files.
-- main.py: contains the test of a same input graph for already implemented algorithm.
-- td.py: contains the implementation for 4k + 4-approximation for treewidth.
+- eord.py: file containing elimination ordering heuristics
--- main.py
+++
@@ -1,9 +0,0 @@
-from sage.all import Graph
-from sage.graphs.graph_decompositions.tree_decomposition import make_nice_tree_decomposition
-
-graph = Graph({0: [1], 1: [0, 2, 3], 2: [1, 4, 5], 3: [1, 5, 6], 4: [2, 5], 5: [2, 3, 4, 6], 6: [3, 5]})
-graph = Graph({0: [1], 1:[0, 2], 2:[1]})
-tw = graph.treewidth()
-tree_decomp = graph.treewidth(certificate=True, k=1, algorithm="sage")
-
-print("Tree decomposition:", tree_decomp.vertices())
